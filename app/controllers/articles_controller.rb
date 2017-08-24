15eb7485fdcee1b21713372d831bbf126dbcfb19
Weston Hinton <wbhinton@gmail.com>
2017-08-24 09:15:40 -0400

    added error page and validations for article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,18 @@ class ArticlesController < ApplicationController
 		end
 	def create 
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to_article_path(@article)
+		if @article.save
+			flash[:notice]="Article was saved"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+
 	end
+	def show
+		@article = Article.find(params[:id])
+	end
+
 
 	private
 	def article_params
